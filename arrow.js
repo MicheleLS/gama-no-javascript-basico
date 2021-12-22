efc7a555dcefcb32bbe93e1a5da429b728b3407c
Michele De Lima Silva <micheledelima40@gmail.com>
2021-12-22 19:18:54 -0300

    pagina
    Criação de uma página.

---
+++ arrow.js
@@ -0,0 +1,14 @@
+function soma(x,y) {
+    return x + y;
+}
+
+const multiplicacao = function(x, y) {
+    return x * y;
+}
+
+const divir = (x,y) => {
+    return x/y;
+
+}
+
+const objeto = () => ({nome: 'JC', sobrenome: "Bombardelli"});
